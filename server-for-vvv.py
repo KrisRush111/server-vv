1c83c2af704904b54e062aa0608580c7a2ffc699
KrisRush111 <rashina.anna@bk.ru>
2025-06-29 20:15:49 +0300

    Update server-for-vvv.py

--- server-for-vvv.py
+++ server-for-vvv.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import pymysql
 import hashlib
+import os
 
 app = Flask(__name__)
 
@@ -57,4 +58,5 @@ def login():
             return jsonify({'status': 'invalid'}), 401
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
